4155751cfde0a16b8203c6a0290d2fca366941fb
Tommy Thorn <tommy-github2@thorn.ws>
2023-09-03 13:18:35 -0700

    wip

--- adder.v
+++ adder.v
@@ -1,23 +1,24 @@
 /*
+ * 20230820, 20230823
  * Simple Null-Convention Logic (NCL) example
  *
- * We a simple ring of a 4-valued NCL variable
+ * We make a simple ring of a 4-valued NCL variable
  */
 `default_nettype none
 module tb;
    reg init = 0;
-   wire [3:0] d;
-   wire [3:0] dn;
+   wire [3:0] d0;
+   wire [3:0] d1;
    wire	      dk;
-   
-   pipeline pipeline_1(init, d, dn, dk);
 
+   pipeline pipeline_1(init, d0, d1, dk);
+
+   always @(posedge dk)
+     $display("got %d", d1);
 
    initial begin
       $dumpfile("pipeline.vcd");
       $dumpvars(0, pipeline_1);
-
-
 
       #10 init = 1;
       #21 init = 0;
@@ -26,108 +27,104 @@ module tb;
 endmodule
 
 module pipeline(input  wire       init,
-		output wire [3:0] d,
-		output wire [3:0] dn,
+		output wire [3:0] d0,
+		output wire [3:0] d1,
 		output wire       dk);
 
    // Each stage consists of registration, computation, and completion detection
 
-   wire [3:0]		     a, b, c, e;
-   wire [3:0]		     an, bn, cn, en;
+   wire [3:0]		     a0, b0, c0, e0;
+   wire [3:0]		     a1, b1, c1, e1;
    wire			     ak, bk, ck, ek;
 
-   wire [3:0]		     s, sn;
-   wire [3:0]		     d_rot  = init ? 4'h1 : s;
-   wire [3:0]		     dn_rot = init ? 4'he : sn;
+   wire [3:0]		     s0, s1;
 
-   adder #(4) adder_i(d, dn, 4'd1, 4'd14, s, sn);
-
-   stage st0(init, e, en,           dk, a, an, ak);
-   stage st3(init, a, an,           ek, d, dn, dk);
-   stage st4(init, d_rot, dn_rot,   ak, e, en, ek);
+   adder #(4) adder_i(d0, d1, 4'd14, 4'd1, s0, s1);
+   stage st0(init, e0, e1 | init, dk, a0, a1, ak);
+   stage st3(init, a0, a1,        ek, d0, d1, dk);
+   stage st4(init, s0, s1,        ak, e0, e1, ek);
 endmodule
 
 module stage(input             init,
 
-	     input [3:0]       a,
-	     input [3:0]       an,
-	     input	       a_acki,
+	     input [3:0]       a0,
+	     input [3:0]       a1,
+	     output            a_comp,
 
-	     output wire [3:0] b,
-	     output wire [3:0] bn,
-	     output reg        b_acko);
-
-   wire			       error = (a & an) != 0;
+	     output wire [3:0] b0,
+	     output wire [3:0] b1,
+	     input	       b_comp);
 
    // registration
    wire			  a_acki_init;
-   wire [3:0]		  ar;
-   wire [3:0]		  anr;
    genvar		  i;
    for (i = 0; i < 4; i = i + 1) begin
-     th22 th22_r1(.A(a[i] & !init), .B(a_acki & !init), .Q(ar[i]));
-     th22 th22_r2(.A(an[i] & !init), .B(a_acki & !init), .Q(anr[i]));
+     th22ni th22_r0(.A(a0[i]), .COMP(k), .INIT(init), .Q(b0[i]));
+     th22ni th22_r1(.A(a1[i]), .COMP(k), .INIT(init), .Q(b1[i]));
    end
 
-   // "computation", we are just rotating the value
-   //always @(*) b = #1 {ar[2:0],ar[3]};
-   assign b = ar;
-   assign bn = anr;
-
    // completion
-   wire [3:0] b_complete = b | bn;
-   wire [3:0] b_error = b & bn;
-   wire [1:0] bb_complete;
-   wire	      bbb_complete;
-   th22 th22_c0(.A(b_complete[0]), .B(b_complete[1]), .Q(bb_complete[0]));
-   th22 th22_c1(.A(b_complete[2]), .B(b_complete[3]), .Q(bb_complete[1]));
-   th22 th22_c2(.A(bb_complete[0]), .B(bb_complete[1]), .Q(bbb_complete));
-   always @(*) b_acko = !bbb_complete;
+   wire [3:0] b_comp1 = b0 | b1;
+   wire [1:0] b_comp2;
+   th22 th22_c0(.A(b_comp1[0]), .B(b_comp1[1]), .Q(b_comp2[0]));
+   th22 th22_c1(.A(b_comp1[2]), .B(b_comp1[3]), .Q(b_comp2[1]));
+   th22 th22_c2(.A(b_comp2[0]), .B(b_comp2[1]), .Q(b_comp));
 endmodule
 
-module adder(a, an, b, bn, r, rn);
-   parameter n = 32;
-   input wire [n-1:0] a;
-   input wire [n-1:0] an;
-   input wire [n-1:0] b;
-   input wire [n-1:0] bn;
-   
-   output wire [n-1:0] r;
-   output wire [n-1:0] rn;
+module adder #(parameter n = 32)
+   (input wire [n-1:0] a0,
+    input wire [n-1:0] a1,
+    input wire [n-1:0] b0,
+    input wire [n-1:0] b1,
+    output wire [n-1:0]	s0,
+    output wire [n-1:0]	s1);
 
-   wire [n-1:0]	       c;
-   wire [n-1:0]	       cn;
+   wire [n-1:0]	       c0, c1;
 
-   add1 add1_0(a[0], an[0], b[0], bn[0], 1'b0, 1'b1, r[0], rn[0], c[0], cn[0]);
-   genvar	       i;
+   add1 add1_0(a0[0], a1[0], b0[0], b1[0], 1'b1, 1'b0, s0[0], s1[0], c0[0], c1[0]);
+   genvar i;
    for (i = 1; i < n; i = i + 1)
-     add1 add1_i(a[i], an[i], b[i], bn[i], c[i-1], cn[i-1], r[i], rn[i], c[i], cn[i]);
+     add1 add1_i(a0[i], a1[i], b0[i], b1[i], c0[i-1], c1[i-1], s0[i], s1[i], c0[i], c1[i]);
 endmodule
 
-module add1(input wire  a,
-	    input wire	an,
-	    input wire	b,
-	    input wire	bn,
-	    input wire	c,
-	    input wire	cn,
+// Dual-rail full-adder
+module add1(input wire	a0,
+	    input wire  a1,
+	    input wire	b0,
+	    input wire	b1,
+	    input wire	ci0,
+	    input wire	ci1,
 
-	    output wire	r,
-	    output wire	rn,
-	    output wire	co,
-	    output wire	con);
+	    // Sum and Carry-out
+	    output wire	s0,
+	    output wire	s1,
+	    output wire	co0,
+	    output wire	co1);
 
-/*
-   wire a_and_bn; th22 th22_1(a, bn, a_and_bn);
-   wire an_and_b; th22 th22_2(an, b, an_and_b);
-   wire a_and_b;  th22 th22_3(a, b, a_and_b);
-*/   
+   // a b ci | co s
+   // 0 0 0  | 0  0
+   // 0 0 1  | 0  1
+   // 0 1 0  | 0  1
+   // 0 1 1  | 1  0
+   // 1 0 0  | 0  1
+   // 1 0 1  | 1  0
+   // 1 1 0  | 1  0
+   // 1 1 1  | 1  1
 
-   // XXX This is broken.  We can't use & but have to use th22 for those
-   assign r = a & b & cn | an & b & cn | an & bn & c | a & b & c;
-   assign rn = an & b & c | a & bn & c | a & b & cn | an & bn & cn;
+   wire i000, i001, i010, i011, i100, i101, i110, i111;
+   th33 th33_0(.Q(i000), .A(a0), .B(b0), .C(ci0));
+   th33 th33_1(.Q(i001), .A(a1), .B(b0), .C(ci0));
+   th33 th33_2(.Q(i010), .A(a0), .B(b1), .C(ci0));
+   th33 th33_3(.Q(i011), .A(a1), .B(b1), .C(ci0));
+   th33 th33_4(.Q(i100), .A(a0), .B(b0), .C(ci1));
+   th33 th33_5(.Q(i101), .A(a1), .B(b0), .C(ci1));
+   th33 th33_6(.Q(i110), .A(a0), .B(b1), .C(ci1));
+   th33 th33_7(.Q(i111), .A(a1), .B(b1), .C(ci1));
 
-   assign co = a & b & (cn|c) | (a|an) & b & c | a & (b|bn) & c;
-   assign con = an & bn & (cn|c) | (a|an) & bn & cn | an & (b|bn) & cn;
+   assign s0  = i110 | i101 | i011 | i000;
+   assign s1  = i001 | i010 | i100 | i111;
+   assign co0 = i100 | i010 | i001 | i000;
+   assign co1 = i011 | i101 | i110 | i111;
 endmodule
 
 
@@ -140,12 +137,22 @@ endmodule
 module th22(input A, input B, output reg Q);
    // We can implement this with a majority gate with feedback
    //sky130_fd_sc_hd__maj3_1 maj(.X(Q), .A(A), .B(B), .C(Q));
+   always @(*) if (A == B) Q <= #1 A;
+endmodule
 
-   always @(*)
-     if (A & B)
-       Q <= #1 1;
-     else if (!A & !B)
-       Q <= #1 0;
+// NULL initialized Threshold 2-of-2 gate with completion (inverted ack)
+module th22ni(input A, input COMP, input INIT, output reg Q);
+   always @(*) if (INIT) Q <= 0; else if (A != COMP) Q <= #1 A;
+endmodule
+
+// DATA initialized Threshold 2-of-2 gate with completion (inverted ack)
+module th22di(input A, input COMP, input INIT, output reg Q);
+   always @(*) if (INIT) Q <= 1; else if (A != COMP) Q <= #1 A;
+endmodule
+
+// Threshold 3-of-3 gate
+module th33(input A, input B, input C, output reg Q);
+   always @(*) if (A == B && B == C) Q <= #1 A;
 endmodule
 
 module sky130_fd_sc_hd__maj3_1 (
@@ -180,7 +187,7 @@ module ncl_or(input [1:0] a, input [1:0] b, output [1:0] q);
    // D(1) D(0) -> D(1)
    // D(1) D(1) -> D(1)
 
-   /* old formulation 
+   /* old formulation
    // This is Fants mapping
    th22 th22_1(.Q(q[0]), .A(a[0]), .B(b[0]));  // When both are 0, output is 0
    th34w32 th34w32_1(.Q(q[1]),                       // a[1]*2 + b[1]*2 + a[0] + b[0] >= 3
@@ -198,7 +205,7 @@ module ncl_or(input [1:0] a, input [1:0] b, output [1:0] q);
    th22 th22_3(.Q(t[3]), .A(a[1]), .B(b[1]));
    assign q = {|t[3:0], t[0]};
 endmodule
-   
+
 module ncl_xor(input [1:0] a, input [1:0] b, output [1:0] q);
    wire [3:0] t;
    th22 th22_0(.Q(t[0]), .A(a[0]), .B(b[0]));
--- ring.v
+++ ring.v
@@ -7,24 +7,69 @@ module top(input clk_25mhz,
 	   output [27:0] gp,
 	   output [27:0] gn);
 
+   parameter N = 4;  // Must be >= 3  (3: 456 MHz, 4: 544, 5: 656 MHz, 6: 512)
+
    // Tie GPIO0, keep board from rebooting
    assign wifi_gpio0 = 1;
 
+   // "User Interface"
+   // btn0 is init
+   // btn1 is go
+   // btn2 is step -- advances once per edge
+   //
+   // The buttons are in priority order and is ignored if a higher
+   // priority button is activated
+   //
+   // The way we "step" the NCL is to only enable one stage to report
+   // completion (hmm, this might not work)
+
    // NCL "reset"
-   wire           init = !btn[0];
+   reg         init = 0;
+   reg	       go   = 0;
+   reg	       step = 0;
+   reg [N-1:0] enable = ~0;
+   reg	       old_step = 0;
+
+   reg [27:0]  clock_divider = 0;
+   
+
+   always @(posedge clk_25mhz) begin
+      init     <= !btn[0];
+      go      <= btn[1];
+      step     <= btn[2];
+
+      clock_divider <= clock_divider - 1;
+      if (clock_divider[27]) begin
+	 clock_divider <= 'd 1_250_000-2;
+	 old_step <= step;
+	 
+	 if (go)
+	   enable <= ~0;
+	 else begin
+	    if (step & !old_step) begin
+	       // Step /
+	       if (&enable)
+		 enable <= 1;
+	       else 
+		 enable <= {enable[N-2:0],enable[N-1]};
+	    end
+	 end
+      end
+   end	
 
    // N-stage ring of N-1 TH22NI and one TH22D
    // The lone th22di feeds q0, and the rest q
    // The actual performance is extremely sensitive to placement/routing so
    // results vary widely, but as a rule, best results are with N=4 or N=5.
-   parameter N = 4;  // Must be >= 3  (3: 456 MHz, 4: 544, 5: 656 MHz, 6: 512)
    wire   [N-2:0] d, q, ack;
    wire           d0, q0, ack0;
 
    assign d0 = q[N-2];
    assign d = {q[N-3:0],q0};      // d[i] = q[i-1 modulo N]
-   assign ack0 = q[0];
-   assign ack = {q0,q[N-2:1]};    // ack[i] = q[i+1 modulo N]
+
+  
+   assign ack0 = enable[0] & q[0];
+   assign ack = enable[N-1:1] & {q0,q[N-2:1]};    // ack[i] = q[i+1 modulo N]
 
    (* keep *) TH22DI th22_x         (.Z(q0),.C(init), .A(d0), .B(ack0));
    (* keep *) TH22NI th22_0 [N-2:0] (.Z(q), .C(init), .A(d),  .B(ack));
@@ -50,6 +95,8 @@ module top(input clk_25mhz,
    assign led = async_led_r;
    always @(posedge async_clock_div16) begin
       async_counter <= async_counter + 1;
-      async_led_r <= async_counter[25:25-7]; // At 600 MHz, this should roll over every 1.79 s
    end
+
+   always @(posedge clk_25mhz)
+      async_led_r <= go ? async_counter[25:25-7] : {ack,ack0,q,q0}; // At 600 MHz, this should roll over every 1.79 s
 endmodule
